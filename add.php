e668402f5a3fce86061765ad2b33d4a4a1eff837
Jauch Walter <walter.jch@eduge.ch>
2020-02-05 16:01:19 +0100

    création base de donnée + changements mineurs php

--- add.php
+++ add.php
@@ -21,7 +21,31 @@
             <div class="uk-card uk-card-default uk-card-body">
                 <h3 class="uk-card-title">Ajouter un post</h3>
                 <p>Veuillez remplir avec les trucs qui vont bien svp hein</p>
-            </div>
+
+                <form>
+    <fieldset class="uk-fieldset">
+
+        <div class="uk-margin">
+            <input class="uk-input" type="text" placeholder="Mon post">
+        </div>
+
+        <div class="uk-margin">
+            <textarea class="uk-textarea" rows="5" placeholder="Textarea"></textarea>
+        </div>
+
+        <div class="js-upload uk-placeholder uk-text-center">
+    <span uk-icon="icon: cloud-upload"></span>
+    <span class="uk-text-middle">Attach binaries by dropping them here or</span>
+    <div uk-form-custom>
+        <input type="file" multiple accept="image/x-png,image/gif,image/jpeg">
+        <span class="uk-link">selecting one</span>
+    </div>
+</div>
+<button class="uk-button uk-button-primary">Primary</button>
+    </fieldset>
+</form>
+
+            </div>            
         </div>
 
         
---
+++ database/conparam.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @remark	Remplir correctement les constantes ci-dessous en fonction de votre base de données
+ */
+
+/*
+ * @brief Connection constants
+ */
+define('EDB_DBTYPE', "mysql");
+define('EDB_DBNAME', "m152_posts");
+define('EDB_HOST', "127.0.0.1");
+define('EDB_PORT', "3306");
+define('EDB_USER', "m152_Admin");
+define('EDB_PASS', "SuperCrocs2012");
+
+
